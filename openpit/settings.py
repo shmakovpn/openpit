9d7910f8f318dbe8b97151d0b7f0fd39abe065a0
shmakovpn <shmakovpn@yandex.ru>
2021-05-13 07:40:15 +0300

    try to change db engine once again

--- openpit/settings.py
+++ openpit/settings.py
@@ -135,7 +135,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
-if os.environ.get('ENV'):
+if os.environ.get('ENV') == 'HEROKU':
     DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Default primary key field type
