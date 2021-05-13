4b43d92575c408388375149d7281b25133ebc098
shmakovpn <shmakovpn@yandex.ru>
2021-05-13 07:34:06 +0300

    try to fix database engine on heroku (postgis)

--- openpit/settings.py
+++ openpit/settings.py
@@ -135,6 +135,8 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 db_from_env = dj_database_url.config(conn_max_age=600, ssl_require=True)
 DATABASES['default'].update(db_from_env)
+if os.environ.get('ENV'):
+    DATABASES['default']['ENGINE'] = 'django.contrib.gis.db.backends.postgis'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
